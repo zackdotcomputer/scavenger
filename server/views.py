5cf1f94963d0127df74449284ca1021e93177809
Zack Sheppard <zack@zacksheppard.com>
2015-05-09 14:30:04 -0400

    Add login_required and privacy policy

--- server/views.py
+++ server/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
   return render(request, 'server/index.html', {})
 
+@login_required
 def profile(request):
   return HttpResponse("Hello, world. You're at the profile page.")
 
+@login_required
 def progress(request):
   return HttpResponse("Hello, world. You're at the progress page.")
