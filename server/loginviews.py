1dab1cc321dc157398c55c669629f3c9cb7de5a7
Zack Sheppard <zack@zacksheppard.com>
2015-05-09 16:25:49 -0400

    Make log in and user creation actually work

--- server/loginviews.py
+++ server/loginviews.py
@@ -1,19 +1,95 @@
-import util
-from django.http import HttpResponseRedirect, HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import logout
+import json
+import logging
+import os
+from . import util
+from .models import Player
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from six import string_types
+
+logger = logging.getLogger('django')
 
 @csrf_exempt
-def login(request):
-  if (request.method == 'POST'):
-    return handleLogin(request)
+def loginPage(request):
+  if (request.user.is_authenticated()):
+    # TODO(zack): Support for the next parameter
+    return HttpResponseRedirect(reverse('profile'))
+
+  hasLoginError = False
 
   foursquare = util.foursquareClient()
-  return HttpResponse(foursquare.oauth.auth_url())
 
-def handleLogin(request):
-  return HttpResponse("Hello, world. You're POSTing to the login page.")
+  if ('code' in request.GET):
+    logger.info("Attempting log in for user")
+    if (handleCodeFlow(request, str(request.GET['code']), foursquare) and
+      ensureUserExistsAndLogin(request, foursquare)):
+      # TODO(zack): Support for the next parameter
+      return HttpResponseRedirect(reverse('profile'))
+    else:
+      hasLoginError = True
+
+  return render(request, 'server/login.html', { 'auth_url': foursquare.oauth.auth_url() })
+
+def handleCodeFlow(request, code, foursquare):
+  access_token = foursquare.oauth.get_token(code)
+  if (isinstance(access_token, string_types) and len(access_token) > 0):
+    logger.info("Successfully logged in")
+    foursquare.set_access_token(access_token)
+    return True
+
+  logger.error("Did not get good Access Token")
+  return False
+
+def ensureUserExistsAndLogin(request, foursquare):
+  resp = foursquare.users()
+
+  if ("user" in resp):
+    me = resp["user"]
+    uid = -1
+    try:
+      uid = long(me["id"])
+    except e:
+      pass
+
+    if uid > 0:
+      existing = None
+      try:
+        existing = Player.objects.get(foursqId=uid)
+      except ObjectDoesNotExist as e:
+        pass
+      password = str(uid) + os.environ['PASSWORD_SECRET'] # never user-facing
+
+      if existing is not None and existing.user.id is not None:
+        user = authenticate(username=existing.user.username, password=password)
+        if user is not None:
+          if user.is_active:
+            login(request, user)
+            return True
+      else:
+        email = me.get('contact', {}).get('email', '')
+        phone = me.get('contact', {}).get('phone', '')
+        theUser = User.objects.create_user(str(uid), email, password)
+        theUser.first_name = me.get('firstName', '')
+        theUser.last_name = me.get('lastName', '')
+        theUser.save()
+
+        thePlayer = existing
+        if thePlayer is None:
+          thePlayer = Player(user=theUser, phone=phone, foursqId=uid)
+        else:
+          thePlayer.user = theUser
+          thePlayer.phone = phone
+
+        thePlayer.save()
+
+        return True
+
+  return False
 
 def handleLogout(request):
   logout(request)
--- server/urls.py
+++ server/urls.py
@@ -20,7 +20,7 @@ from . import views, loginviews, pushlanding
 urlpatterns = [
   url(r'^admin/', include(admin.site.urls)),
   url(r'^$', views.index, name='index'),
-  url(r'^login/?$', loginviews.login, name='login'),
+  url(r'^login/?$', loginviews.loginPage, name='login'),
   url(r'^logout/?$', loginviews.handleLogout, name='logout'),
   url(r'^pushlanding$', pushlanding.handle, name='pushlanding'),
   url(r'^profile/?$', views.profile, name='profile'),
