d171a745c4045d2513c52fe52a7d9db257e252c7
Zack Sheppard <zack@zacksheppard.com>
2015-05-09 14:36:52 -0400

    Move secret key into heroku config (and rotate)

--- server/settings.py
+++ server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n$hnr%t6t*nk0#w-$4t8a8g)at82)=u%ssd0)34_)z%3&ey!bi'
+SECRET_KEY = os.environ['DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
